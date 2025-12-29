7c1e8a1c281560da6b49a13bcdda8dc72288a44d
Kwame Opoku Agyeman <Kwameagyeman22@yahoo.com>
2025-12-29 21:32:21 +0000

    Update Mortgage.py

--- Mortgage.py
+++ Mortgage.py
@@ -1,17 +1,27 @@
+# Actually this is the first Mortgage Calculator I made.
+# I made a 2nd Mortgage calculator because I used Simple interest for the whole mortgage process instead of compound interest
+# This makes the whole project unaccurate / unreliable or wrong
+
 print("Welcome to this Mortgage calculator")
 
 def mortgage(c, deposit):
+# c is house price, deposit is th
     return c - deposit
 
 
 # noinspection PyTypeChecker
 def total_mortgage(mt: object, interest: object) -> object:
+# mt is mortgage, interest is annual value 
     return mt +((mt * interest) / 100)
 
 def fixed_deposit(tm, time):
+#tm is total mortgage(with interest), time is period to pay(usually 25 years)
     return (tm / time)/12
 
 def overpayment_amount(fd,oa,tm):
+#fd is monthly payment,oa is overpayment, total mortgage
+# Overpayment is the extra money paid in addition to the monthly payment
+# the reason is to reduce the number of years and the interest it takes to pay off the mortgage
     return tm/((fd+oa)*12)
 
 print("1. Mortgage(Without interest)")
@@ -61,6 +71,7 @@ while True:
         oa = float(input("Overpayment amount: "))
         print(f"The years left using overpayment is {round(overpayment_amount(fd,oa,tm))} 4"
               f"years")
+
 
 
 
